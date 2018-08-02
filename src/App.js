3d37a2afdaf1abc17f473421e0a1d9635f1af2b5
Danne Woo <dannewoo@Dannes-MacBook-Air.local>
2018-08-02 15:19:06 -0400

    Cleaned up code and added some styling.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Reps from './Reps';
 
@@ -12,7 +11,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="remix-button">
+          <a href="https://glitch.com/edit/#!/remix/pushing-the-line" target="_blank">
+            <img src="https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fremix%402x.png?1513093958726" alt="remix button" aria-label="remix" height="25" />
+          </a>
+        </div>
         <div className="App-header">
+          <img className="logo" src="COC-Full-Color_320px.png" />
           <h2>Welcome to pushing the line</h2>
         </div>
         <Reps/>
